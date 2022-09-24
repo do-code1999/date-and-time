09a4108b1210409e63fe62294c066fa9fe145d50
do-code1999 <112854438+do-code1999@users.noreply.github.com>
2022-09-24 12:08:34 +0800

    Add files via upload

---
+++ button datetime/script.js
@@ -0,0 +1,19 @@
+/*
+const car = { brand: "Bugatti",
+              model: "Bolide",
+              year:  2021,
+              engine: "W16",
+              color: "Black",
+              paint: function()
+              {
+                return this.color;
+              }
+            };
+
+const car_color = { color: "Blue"}
+
+console.log("I have a " + car["brand"] + " " + car.model + " that was released recently in " + car.year + 
+            " with " + car["engine"] + " engine.\nPainted with " + car.paint.apply(car_color) + ".");
+*/
+
+button.style.color = "white";
